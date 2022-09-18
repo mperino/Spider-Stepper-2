a7b3472b4d23d931fbe83504137effc0e4f817ed
mperino <mark.perino@gmail.com>
2022-09-17 18:37:20 -0700

    Add files via upload
    Faster, better, cleaner...

--- code.py
+++ code.py
@@ -53,15 +53,15 @@ if stepper4 == True:
 #Define steps per rotation
 stepsperrot = 2048
 
-#Define forward and backwards RPM (max is less than abs(20)) Use negative RPM to move the stepper backwards,
+#Define forward and backwards RPM (max is less than abs(16)) Use negative RPM to move the stepper backwards,
 # Do not use negative steps to move things backwards.
-forwardRPM=8
-backwardsRPM= -10
+forwardRPM=16
+backwardsRPM= -16
 #Define how many steps. or use rotations (stepsperrot * #) to lower/raise at start and end
 lowersteps = stepsperrot * 1
 #define how many steps to cycle up and down after initial lower the "twitching"
-twitchsteps = 90
-twitches = 4
+twitchsteps = 60
+twitches = 8
 
 #define how long to pause at end before re-starting
 sleeptime = 5
@@ -70,8 +70,6 @@ sleeptime = 5
 cyclesteps = 4
 
 # Other notes
-# it is not usually neccisary to apply a holding power to these weak steppers. They generally have enough friction to
-# hold without any applied power.  So the function stopall() is applied to moveallatrpm(steps, rpm).
 # You should not need to edit anything bellow this point for most use cases, but hopefully the code is clear enough to
 # be easily grok'd...
 
@@ -79,7 +77,7 @@ cyclesteps = 4
 # Functions to translate steps into co-ordinated rotation.
 # This is pure bit-banging with time sleeping to get RPM's (which will be inaccurate and always a little slower)
 # ==============================================================================================
-
+#
 def statefromsteppin(pin, step):
     #   step 1 2 3 4
     # pin 1  1 1 0 0
@@ -95,95 +93,52 @@ def statefromsteppin(pin, step):
     else:
         return False
 
-def rotatestepsatrpm(steppernum, steps, rpm):
+def rotateallatrpm(steps,rpm):
     if rpm < 0:
         steps = steps * -1
     if steps >= 1:
         for i in range(1, steps + 1):
-            firepins(steppernum,i)
+            fireallpins(i)
             time.sleep(60 / (abs(rpm) * stepsperrot))
     if steps <= 1:
         for i in range(-1, steps - 1, -1):
-            firepins(steppernum, i)
+            fireallpins(i)
             time.sleep(60 / (abs(rpm) * stepsperrot))
 
-def firepins(steppernum, nextstep):
-#    print("Step:{}".format(nextstep))
-    if steppernum == 1 and stepper1 == True:
+def fireallpins(nextstep):
+    if stepper1 == True:
         pin11.value = statefromsteppin(1, nextstep)
         pin12.value = statefromsteppin(2, nextstep)
         pin13.value = statefromsteppin(3, nextstep)
         pin14.value = statefromsteppin(4, nextstep)
-    elif steppernum == 2 and stepper2 == True:
+    if stepper2 == True:
         pin21.value = statefromsteppin(1, nextstep)
         pin22.value = statefromsteppin(2, nextstep)
         pin23.value = statefromsteppin(3, nextstep)
         pin24.value = statefromsteppin(4, nextstep)
-    elif steppernum == 3 and stepper3 == True:
+    if stepper3 == True:
         pin31.value = statefromsteppin(1, nextstep)
         pin32.value = statefromsteppin(2, nextstep)
         pin33.value = statefromsteppin(3, nextstep)
         pin34.value = statefromsteppin(4, nextstep)
-    elif steppernum == 4 and stepper4 == True:
+    if stepper4 == True:
         pin41.value = statefromsteppin(1, nextstep)
         pin42.value = statefromsteppin(2, nextstep)
         pin43.value = statefromsteppin(3, nextstep)
         pin44.value = statefromsteppin(4, nextstep)
-    else:
-        print("no valid stepper number or stepper is not enabled.")
-
-def stopstepper(steppernum):
-    if steppernum == 1 and stepper1 == True:
-        pin11.value = statefromsteppin(1, False)
-        pin12.value = statefromsteppin(2, False)
-        pin13.value = statefromsteppin(3, False)
-        pin14.value = statefromsteppin(4, False)
-    elif steppernum == 2 and stepper2 == True:
-        pin21.value = statefromsteppin(1, False)
-        pin22.value = statefromsteppin(2, False)
-        pin23.value = statefromsteppin(3, False)
-        pin24.value = statefromsteppin(4, False)
-    elif steppernum == 3 and stepper3 == True:
-        pin31.value = statefromsteppin(1, False)
-        pin32.value = statefromsteppin(2, False)
-        pin33.value = statefromsteppin(3, False)
-        pin34.value = statefromsteppin(4, False)
-    elif steppernum == 4 and stepper4 == True:
-        pin41.value = statefromsteppin(1, False)
-        pin42.value = statefromsteppin(2, False)
-        pin43.value = statefromsteppin(3, False)
-        pin44.value = statefromsteppin(4, False)
-    else:
-        print("no valid stepper number or stepper is not enabled.")
-def moveallatrpm(steps, rpm):
-    remainingsteps = steps
-    while remainingsteps > 0:
-        dosteps = min(cyclesteps, remainingsteps)
-        if stepper1 == True:
-            rotatestepsatrpm(1, dosteps, rpm)
-        if stepper2 == True:
-            rotatestepsatrpm(2, dosteps, rpm)
-        if stepper3 == True:
-            rotatestepsatrpm(3, dosteps, rpm)
-        if stepper4 == True:
-            rotatestepsatrpm(4, dosteps, rpm)
-        remainingsteps = remainingsteps - cyclesteps
-    stopall()
-
-def stopall():
-    for i in range(1,5):
-        stopstepper(i)
 
 #===================MAIN===============================
 while True:
     #lower, twitch, raise all spiders
     print("lowering all")
-    moveallatrpm(lowersteps,forwardRPM)
+    start =time.time()
+    rotateallatrpm(lowersteps,forwardRPM)
+    print("elapsed",time.time()-start)
     print("Twitching")
     for i in range(1,twitches +1):
-        moveallatrpm(twitchsteps, forwardRPM)
-        moveallatrpm(twitchsteps, backwardsRPM)
+        rotateallatrpm(twitchsteps, forwardRPM)
+        rotateallatrpm(twitchsteps, backwardsRPM)
     print("Raise All")
-    moveallatrpm(lowersteps, backwardsRPM)
+    rotateallatrpm(lowersteps, backwardsRPM)
     print("Sleep")
 
